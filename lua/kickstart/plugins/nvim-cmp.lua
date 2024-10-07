010a037faf3cd91feb1d4e27b962dab967da7a93
MimiValsi <miguel@dasilvaf.net>
2024-10-07 15:10:49 +0200

    Change autocomplete shortcut

--- lua/kickstart/plugins/nvim-cmp.lua
+++ lua/kickstart/plugins/nvim-cmp.lua
@@ -63,12 +63,12 @@ return { -- Autocompletion
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
 
         ['<C-Space>'] = cmp.mapping.complete {},
-        ['<C-y>'] = cmp.mapping.confirm {
+        ['<Tab>'] = cmp.mapping.confirm {
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
         },
 
-        ['<C-j>'] = cmp.mapping(function(fallback)
+        ['<C-l>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
           elseif luasnip.expand_or_locally_jumpable() then
@@ -78,7 +78,7 @@ return { -- Autocompletion
           end
         end, { 'i', 's' }),
 
-        ['<C-k>'] = cmp.mapping(function(fallback)
+        ['<C-h>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
           elseif luasnip.locally_jumpable(-1) then
