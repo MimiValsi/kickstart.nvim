deb5d3f8b7bc02b08d8225d356d93f452ac75f1a
Miguel Da Silva <miguel@dasilvaf.net>
2024-10-18 15:41:09 +0200

    Change save file shortcut, and quit shortcut

--- lua/kickstart/remaps.lua
+++ lua/kickstart/remaps.lua
@@ -12,11 +12,14 @@ map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-map('n', '<leader>w', ':w<cr>', { desc = 'Save file' })
+map('n', '<leader>ww', ':w<cr>', { desc = 'Save file' })
+map('n', '<leader>we', ':q<cr>', { desc = 'Quit file' })
+
 map('n', ';', ':', { desc = 'Cmd shortcut' })
 
 map('n', '<C-i>', ':tabn<cr>', { desc = 'Go to next tab' })
 map('n', '<C-o>', ':tabp<cr>', { desc = 'Go to prev tab' })
+map('n', '<leader>t', ':tab ter<cr>', { desc = 'New tab with terminal' })
 
 map('n', '<leader>ra', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', { desc = 'replace the word everywhere in the file/buffer' })
 
