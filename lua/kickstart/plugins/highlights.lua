2ce39efff71755c5f63921c6e4e0463c7dace679
Miguel Da Silva <miguel@dasilvaf.net>
2024-12-08 17:08:31 +0100

    exclude text files from treesitter & highlights colors

--- lua/kickstart/plugins/highlights.lua
+++ lua/kickstart/plugins/highlights.lua
@@ -2,6 +2,9 @@ return {
   'brenoprata10/nvim-highlight-colors',
 
   config = function()
-    require('nvim-highlight-colors').setup {}
+    require('nvim-highlight-colors').setup {
+      exclude_filetypes = { 'text' },
+      exclude_buftypes = { 'text' },
+    }
   end,
 }
--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -6,6 +6,8 @@ return {
   opts = {
     ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go', 'zig' },
 
+    ignore_install = { 'text' },
+
     auto_install = true,
     highlight = {
       enable = true,
