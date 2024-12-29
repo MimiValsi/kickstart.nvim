dfd57bb542da259156813f8e76f91d37cee5720c
Miguel DA SILVA <miguel@dasilvaf.net>
2024-12-29 14:46:14 +0100

    change term exit shortcut

--- lua/kickstart/remaps.lua
+++ lua/kickstart/remaps.lua
@@ -61,4 +61,4 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-map('t', '<C- >', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+map('t', '<C-c>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
