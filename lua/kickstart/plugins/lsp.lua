13f73c1dea09158a1de6862c4b4a0f76faef89d8
Miguel DA SILVA <miguel@dasilvaf.net>
2025-01-19 21:09:15 +0100

    Remove zls

--- lua/kickstart/plugins/lsp.lua
+++ lua/kickstart/plugins/lsp.lua
@@ -149,7 +149,7 @@ return {
     --  - cmd (table): Override the default command used to start the server
     --  - filetypes (table): Override the default list of associated filetypes for the server
     --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-    --  - settings (table): Override the default settings passed when initializing the server.
+    --  - settings (table): Override the default settings passed when initializing the server.lsp
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       gopls = {
@@ -183,8 +183,6 @@ return {
       },
 
       cssls = {},
-
-      zls = {},
 
       lua_ls = {
         -- cmd = {...},
