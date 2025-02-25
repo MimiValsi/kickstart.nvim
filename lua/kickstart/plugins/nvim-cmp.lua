0b57780106a71a3164386a4f0a3c719b8cc6c68a
Miguel Da Silva <miguel@dasilvaf.net>
2025-02-25 17:16:29 +0100

    add minimal functionality for snippets behaviour

--- lua/kickstart/plugins/nvim-cmp.lua
+++ lua/kickstart/plugins/nvim-cmp.lua
@@ -52,45 +52,43 @@ return { -- Autocompletion
       -- chosen, you will need to read `:help ins-completion`
       --
       -- No, but seriously. Please read `:help ins-completion`, it is really good!
-      -- mapping = cmp.mapping.preset.insert {
-      --   -- Select the [n]ext item
-      --   ['<C-n>'] = cmp.mapping.select_next_item(),
-      --   -- Select the [p]revious item
-      --   ['<C-p>'] = cmp.mapping.select_prev_item(),
-      --
-      --   -- Scroll the documentation window [b]ack / [f]orward
-      --   ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      --   ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      --
-      --   ['<C-Space>'] = cmp.mapping.complete {},
-      --   ['<Tab>'] = cmp.mapping.confirm {
-      --     behavior = cmp.ConfirmBehavior.Replace,
-      --     select = true,
-      --   },
-      --
-      --   ['<C-j>'] = cmp.mapping(function(fallback)
-      --     -- if cmp.visible() then
-      --     --   cmp.select_next_item()
-      --     if luasnip.expand_or_locally_jumpable() then
-      --       luasnip.expand_or_jump()
-      --     else
-      --       fallback()
-      --     end
-      --   end, { 'i', 's' }),
-      --
-      --   ['<C-k>'] = cmp.mapping(function(fallback)
-      --     -- if cmp.visible() then
-      --     --   cmp.select_prev_item()
-      --     if luasnip.locally_jumpable(-1) then
-      --       luasnip.jump(-1)
-      --     else
-      --       fallback()
-      --     end
-      --   end, { 'i', 's' }),
-      --
-      --   -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-      --   --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-      -- },
+      mapping = cmp.mapping.preset.insert {
+        -- Select the [n]ext item
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        -- Select the [p]revious item
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+
+        -- Scroll the documentation window [b]ack / [f]orward
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        --
+        --   ['<C-Space>'] = cmp.mapping.complete {},
+        ['<C-e>'] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        },
+
+        ['<C-j>'] = cmp.mapping(function(fallback)
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+
+        --   ['<C-k>'] = cmp.mapping(function(fallback)
+        --     -- if cmp.visible() then
+        --     --   cmp.select_prev_item()
+        --     if luasnip.locally_jumpable(-1) then
+        --       luasnip.jump(-1)
+        --     else
+        --       fallback()
+        --     end
+        --   end, { 'i', 's' }),
+        --
+        --   -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+        --   --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+      },
       sources = {
         {
           name = 'lazydev',
