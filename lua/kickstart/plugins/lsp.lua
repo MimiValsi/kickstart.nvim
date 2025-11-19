a8b73b2490f9aaeb01ce808b5df73cadb99cf9bc
Miguel Da Silva Ferreira <miguel@dasilvaf.net>
2025-11-19 22:32:24 +0100

    add some lsp/format ensure installed

--- lua/kickstart/plugins/lsp.lua
+++ lua/kickstart/plugins/lsp.lua
@@ -169,6 +169,9 @@ return {
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
+      'zls',
+      'clangd',
+      'gopls',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
